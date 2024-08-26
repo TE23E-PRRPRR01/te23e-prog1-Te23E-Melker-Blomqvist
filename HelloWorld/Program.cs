998f69b2acea3f98926dba8e947d5094f8a4b9f2
Melker Blomqvist <melker.blomqvist@elev.ga.ntig.se>
2024-08-26 14:34:36 +0200

    Jag har tränat på CR, CW,

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Här är mitt första c#-program");
+
+Console.WriteLine("Vad heter ni?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn);
+Console.WriteLine("Vad gillar du att göra på fritiden " + namn);
+string fritiden = Console.ReadLine();
+Console.WriteLine("skull emoji");
+Console.BackgroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Såg du på OS i sommar?");
+Console.WriteLine("Vilken sport gillar du mest?");
---
+++ Kapitel 1/registrering/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registrera förnamn och efternamn ");
+Console.WriteLine("Ange förnamn");
+string förnamn = Console.ReadLine();
+Console.WriteLine("Ange efternamn: ");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Hej! " + förnamn + " " + efternamn);
+Console.WriteLine($"Hej! {förnamn} {efternamn}");
