558aa060af8edbdca424d4b03b4780eaa52b5c57
Melker Blomqvist <melker.blomqvist@elev.ga.ntig.se>
2024-09-02 14:51:47 +0200

    Tränat på "if" satser

---
+++ Kapitel 3/EngChart/Program.cs
@@ -0,0 +1,60 @@
+﻿// Program som simulerar kända Engineering Flowchart
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörens flödesschema");
+
+// Visa mig en ASCI art
+Console.WriteLine($"""
+                       -   ,--_--.
+               -           \      `.
+                      -     "-_ _   \
+     -                         / F   )
+                   -     -    / / `--'
+              -              / /
+                   -        / /
+            -            __/ /
+                        /,-pJ
+           -        _--"-L ||
+                  ,"      "//
+     -           /  ,-"".//\
+                /  /     // J____
+               J  /     // L/----\
+   .           F J     //__//^---'
+     `     ___J  F    '----| |
+  `       J---|  F         F F
+`   `. `   `--J  L        J  F
+    .   .`     L J       J  F
+       .  .    J  \    ,"  F
+         .  `.` \  "--"  ,"
+    hs      ` ``."-____-"
+""");
+
+Console.WriteLine("Does it move? (Y/N) ");
+string answer = Console.ReadLine();
+
+// Check if answer is yes or no
+if (answer == "Y")
+{ // Y
+    Console.WriteLine("Should it? (Y/N)");
+    answer = Console.ReadLine();
+    if (answer == "Y")
+    {
+        Console.WriteLine("No problem");
+    }
+    else
+    {
+        Console.WriteLine("Please use duct tape");
+    }
+}
+else
+{ // N
+    Console.WriteLine("Should it? (Y/N)");
+    answer = Console.ReadLine();
+    if (answer == "Y")
+    {
+        Console.WriteLine("Please use WD-40!");
+    }
+    else
+    {
+        Console.WriteLine("No problem!");
+    }
+}
---
+++ Kapitel 3/InteractivStory/Program.cs
@@ -0,0 +1,35 @@
+﻿// Program InteractiveStory
+Console.Clear();
+Console.WriteLine("Welcome to my dungeon game");
+
+Console.WriteLine("You've arrived at a dungeon, enter? (Y/N)");
+string answer = Console.ReadLine();
+
+// Check if answer is yes or no
+if (answer == "Y")
+{ // Y
+    Console.WriteLine("You are inside the opening of the cave, turn right or left? (R/L)");
+    answer = Console.ReadLine();
+    if (answer == "R")
+    {
+        Console.WriteLine("You meet with a goblin, do you run or fight? (Run/Fight)");
+    }
+    else
+    {
+        Console.WriteLine("You find a locked door and a knife on the floor, look for a key or turn back? (look for a key/turn back)");
+    }
+}
+else
+{ // N
+    Console.WriteLine("You decide to turn back, but you get lost in the forest (Try to get out/Set up camp");
+    answer = Console.ReadLine();
+    if (answer == "Try to get out")
+    {
+        Console.WriteLine("You find yourself back on a different path, surrounded by fog. (Follow path/Follow path the other way.)");
+    }
+    else
+    {
+        Console.WriteLine("You sleep for the night but wake up surrounded by goblins. (Hide/Fight them)");
+    }
+}
+
