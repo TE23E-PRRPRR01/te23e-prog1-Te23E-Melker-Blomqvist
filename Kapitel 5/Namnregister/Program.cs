205adefc64d1f0d2825298af48aec6ebb0572ea3
Melker Blomqvist <melker.blomqvist@elev.ga.ntig.se>
2024-10-14 15:03:54 +0200

    Else if, foreach. Namnlistor

---
+++ Kapitel 5/Namnregister/Program.cs
@@ -0,0 +1,43 @@
+﻿// Ett litet namnregister
+Console.Clear();
+Console.WriteLine("Ett litet namnregister");
+
+// Programloopen
+List<string> namnlista = [];
+while (true)
+{
+
+
+
+    // Visa en meny
+    Console.WriteLine("""
+1. Registrera alla namn
+2. Skriva ut alla namn
+3. Avsluta 
+Ange ditt val: 
+""");
+    string val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        Console.Write("Ange ett namn: ");
+        string namn = Console.ReadLine();
+        namnlista.Add(namn);
+
+    }
+    else if (val == "2")
+    {
+        // skriv ut namnen
+        foreach (var namnet in namnlista)
+        {
+            Console.Write($"{namnet}, ");
+        }
+        Console.WriteLine();
+    }
+
+    if (val == "3")
+    {
+        break;
+    }
+
+}
---
+++ Kapitel 5/listor/Program.cs
@@ -0,0 +1,47 @@
+﻿// Använda listor för att skapa/hantera samlingar
+Console.Clear();
+
+// Lista på trevliga frukter. t.ex "äpple, "apelsin, "annanas"
+List<string> frukter = ["äpple", "banan" , "päron"];
+
+Console.WriteLine($"Min fruktlista: {frukter[0]}, {frukter[1]}, {frukter[2]},");
+
+// ändra
+frukter[0] = "apelsin";
+Console.WriteLine($"Min fruktlista: {frukter[0]}, {frukter[1]}, {frukter[2]},");
+
+// Gå igenom listan = loopa igenom = från första till sista
+foreach (var frukt in frukter)
+{
+    Console.WriteLine(frukt);
+}
+
+// Skapa en lista på valfri sak (4st)
+List<string> maträtter = ["Lasange", "Hamburgare", "Korv", "Pannkakor"];
+
+// Skriv ut  varje sak på samma rad
+foreach (var maträtt in maträtter)
+{
+    Console.WriteLine($"{maträtt }");
+}
+
+// Skapa ett register
+Console.Clear();
+
+// En tom lista
+List<string> namnlista = [];
+
+// while-loop
+while (true)
+{
+    fonsole.Write("Ange ett namn: ");
+    string namn = Console.ReadLine();
+    namnlista.Add(namn);
+
+    // skriv ut hela listan på en rad
+    foreach (var namnet in namnlista)
+    {
+        Console.Write($"{namnet}, ");
+    }
+    Console.WriteLine();
+}
