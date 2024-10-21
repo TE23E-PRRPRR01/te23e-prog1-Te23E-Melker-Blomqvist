3eda0ab2454ee9055c45297b9895043e21da4184
Melker Blomqvist <melker.blomqvist@elev.ga.ntig.se>
2024-10-21 14:51:46 +0200

    tränat på index, foreach

---
+++ Kapitel 5/CeasarKrypto/Program.cs
@@ -0,0 +1,47 @@
+﻿// Ett program för kryptering av ceasar-chiffer
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Ett program för kryptering av ceasar-chiffer");
+
+// Alfabetet
+string Alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
+
+// Ange ett meddelande
+Console.WriteLine("Ange ett meddelande: ");
+string meddelande = Console.ReadLine().ToUpper();
+
+// Ange en nyckel
+Console.WriteLine("Ange en nyckel (1-9)");
+int nyckel = int.Parse(Console.ReadLine());
+
+// Loop
+foreach (char bokstav in meddelande)
+{
+    // Hitta bokstavens position (index)
+    int index = Alfabetet.IndexOf(bokstav);
+    //Console.WriteLine($"'A' så är index={index}");
+
+    if (index != -1)
+    {
+        // Caesar kryptering, addera en nyckel
+        int nyIndex = index + nyckel;
+        //Console.WriteLine($"{index} + {nyckel} = {nyIndex}");
+
+        // Börja om alfabet om nyckeln går ur listan.
+        if (nyIndex >= Alfabetet.Length)
+        {
+            nyIndex = nyIndex - Alfabetet.Length;
+        }
+
+        // Plocka ut bokstaven för nyIndex  
+        char krypteradBokstav = Alfabetet[nyIndex];
+        //Console.WriteLine($"{nyIndex} är bokstaven {krypteradBokstav}");
+        Console.Write(krypteradBokstav);
+
+    }
+    else
+    {
+        Console.Write(bokstav);
+    }
+}
+
